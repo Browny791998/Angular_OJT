6bb0b86bea3c0cb0455abee03a646ac44367369f
Ye Htet <yha791998@gmail.com>
2022-05-17 15:12:26 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tutorial';
+  constructor(private router: Router,private fb:FormBuilder) { }
+  showNav = 'none';
+  hideLogin='';
+  Login(data:any){
+    if (data.email=="admin" && data.password=="123") {
+      alert("Login Successful");
+      this.router.navigate(['users'])
+      this.showNav='';
+   this.hideLogin='none';
+   this.router.navigate(["users"]);
+   } else {
+      alert("Invalid Login");
+      this.router.navigate(['logout'])
+   }
+  
+  }
+  Logout(){
+    this.showNav='none';
+     this.hideLogin='';
+    }
+
+    get email() {
+      return this.loginForm?.get('email');
+    }
+    get password() {
+      return this.loginForm?.get('password');
+    }
+
+    loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
+      password:['',Validators.required]
+    })
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialExampleModule } from 'material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserListComponent } from './user-list/user-list.component';
+import { RegisterFormComponent} from './register-form/register-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+ RegisterFormComponent,
+ LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
